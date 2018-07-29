aeaea5618a2e7b01c7eec8d139872f20c8217a8a
1053910251 <1053910251@qq.com>
2018-07-29 22:14:05 +0800

    登录成功设置cookie

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import {Body, Controller, Post} from '@nestjs/common';
+import {Body, Controller, Post, Res} from '@nestjs/common';
 import {AuthService} from './auth.service';
 import {AuthLoginDto} from './dto/auth-login.dto';
 
@@ -9,7 +9,12 @@ export class AuthController {
     ) {}
 
     @Post('token')
-    async createToken(@Body() loginDto: AuthLoginDto): Promise<any> {
-        return await this.authService.createToken(loginDto);
+    async createToken(@Body() loginDto: AuthLoginDto, @Res() response): Promise<any> {
+        const token = await this.authService.createToken(loginDto);
+        response
+            .cookie('token', token, {
+                maxAge: 60 * 60 * 1000,
+            })
+            .json(token);
     }
 }
