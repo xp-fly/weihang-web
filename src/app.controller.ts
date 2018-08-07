6a08d3c99a3aa2dbdbbee14e7e16aea949801870
xiepengfei <xiepengfei@wezhuiyi.com>
2018-08-07 20:01:01 +0800

    解决tslint报错

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,6 @@
 import {Get, Controller, Res} from '@nestjs/common';
 import { AppService } from './app.service';
 import * as path from 'path';
-import {readFile} from 'fs';
 
 @Controller()
 export class AppController {
@@ -13,7 +12,7 @@ export class AppController {
   }
 
   @Get('index')
-  async renderIndex(@Res() response: Response): Promise<any> {
+  async renderIndex(@Res() response: any): Promise<any> {
     const filePath = path.resolve(__dirname, '../../weihang_view/dist/index.html');
     response.sendFile(filePath);
   }
